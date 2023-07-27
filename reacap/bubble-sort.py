22af0e85bb8e1a6267f647a4e3a360780276de87
princebillygk <princebillygk@gmail.com>
2023-07-27 08:12:55 +0600

    practiced one problem with bubble sort but got ttl

--- reacap/bubble-sort.py
+++ reacap/bubble-sort.py
@@ -4,6 +4,5 @@ for i in range(len(arr) - 1):
     for j in range(len(arr) - i - 1):
         if arr[j] > arr[j+1]:
             arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    print("iteration: ", i,  " arr: ", arr)
 
 print("result: ", arr)
