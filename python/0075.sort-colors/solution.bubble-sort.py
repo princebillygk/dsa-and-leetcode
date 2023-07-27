ce7f37c130671d08a32e29691d1f0764b50519cd
princebillygk <princebillygk@gmail.com>
2023-07-27 08:23:41 +0600

    lol

--- python/0075.sort-colors/solution.bubble-sort.py
+++ python/0075.sort-colors/solution.bubble-sort.py
@@ -10,7 +10,10 @@ from leetgo_py import *
 
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        nums.sort()
+        for i in range(len(nums) - 1):
+            for j in range(len(nums) - 1 - i):
+                if nums[j] > nums[j+1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
 
         # @lc code=end
 
