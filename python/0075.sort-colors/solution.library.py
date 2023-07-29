443ae891b71c9a5b06bbe5b50884e61d69cc9af6
princebillygk <princebillygk@gmail.com>
2023-07-29 09:35:55 +0600

    solved sort-colors problem with merge sort

--- python/0075.sort-colors/solution.library.py
+++ python/0075.sort-colors/solution.library.py
@@ -1,8 +1,8 @@
 # Created by princebillygk at 2023/07/27 06:13
 # leetgo: dev
 # https://leetcode.com/problems/sort-colors/
-# Runtime 51 ms Beats 50.21%
-# Memory 16.4 MB Beats 5.9%
+# Runtime 48 ms Beats 67.47%
+# Memory 16.3 MB Beats 72.67%
 
 from typing import *
 from leetgo_py import *
@@ -12,10 +12,7 @@ from leetgo_py import *
 
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        for i in range(len(nums) - 1):
-            for j in range(len(nums) - 1 - i):
-                if nums[j] > nums[j+1]:
-                    nums[j], nums[j+1] = nums[j+1], nums[j]
+        nums.sort()
 
         # @lc code=end
 
--- python/0075.sort-colors/solution.py
+++ python/0075.sort-colors/solution.py
@@ -1,6 +1,8 @@
 # Created by princebillygk at 2023/07/27 06:13
 # leetgo: dev
 # https://leetcode.com/problems/sort-colors/
+# Runtime 42ms Beats 90.89%
+# Memory 16.43mb Beats 5.09%
 
 from typing import *
 from leetgo_py import *
@@ -9,8 +11,26 @@ from leetgo_py import *
 
 
 class Solution:
+    @staticmethod
+    def merge(nums1: List[int], nums2: List[int], nums: List[int]):
+        i = j = 0
+        while i + j < len(nums):
+            if j == len(nums2) or (i < len(nums1) and nums1[i] < nums2[j]):
+                nums[i + j] = nums1[i]
+                i += 1
+            else:
+                nums[i + j] = nums2[j]
+                j += 1
+
     def sortColors(self, nums: List[int]) -> None:
-        nums.sort()
+        if len(nums) < 2:
+            return
+        pivot = len(nums) // 2
+        nums1 = nums[: pivot]
+        nums2 = nums[pivot:]
+        self.sortColors(nums1)
+        self.sortColors(nums2)
+        Solution.merge(nums1, nums2, nums)
 
         # @lc code=end
 
