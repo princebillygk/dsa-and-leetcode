c8db3bca709d2666141b0503b6042758360ef749
princebillygk <princebillygk@gmail.com>
2023-07-31 08:10:45 +0600

    leanred quick sort

--- recap/dsa/quicksort.py
+++ recap/dsa/quicksort.py
@@ -1,5 +1,20 @@
 def quicksort(array):
-    return []
+    pivotIdx = len(array) - 1
+    if (pivotIdx < 1):
+        return array
+    pivotVal = array[pivotIdx]
+
+    i = 0
+    while i < pivotIdx:
+        if array[i] > pivotVal:
+            array[pivotIdx] = array[i]
+            pivotIdx -= 1
+            array[i] = array[pivotIdx]
+            array[pivotIdx] = pivotVal
+        else:
+            i += 1
+
+    return quicksort(array[:pivotIdx]) + [pivotVal] + quicksort(array[pivotIdx + 1:])
 
 
 test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
