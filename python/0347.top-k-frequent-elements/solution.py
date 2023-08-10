79a63c1615c9d719a297fd09884c8a7ef395d587
princebillygk <princebillygk@gmail.com>
2023-08-10 06:11:46 +0600

    solved kth frequency using quick sort

--- python/0347.top-k-frequent-elements/solution.py
+++ python/0347.top-k-frequent-elements/solution.py
@@ -6,17 +6,30 @@ from typing import *
 from leetgo_py import *
 
 # @lc code=begin
+# TODO do this in merge sort and priority queue/heap solution
 
 T = TypeVar("T")
 
 
-def quickSort(ll: list[T]):
-    print(ll)
-    pass
+def quickSort(ll: list[T], compareFn: Callable[[T, T], bool], start: int, end: int):
+    if (start > end):
+        return
+
+    pivotIdx = end
+    i = start
+    while i < pivotIdx:
+        if compareFn(ll[i], ll[pivotIdx]):
+            ll[pivotIdx], ll[i] = ll[i], ll[pivotIdx]
+            pivotIdx -= 1
+            ll[pivotIdx], ll[i] = ll[i], ll[pivotIdx]
+        else:
+            i += 1
+
+    quickSort(ll, compareFn, start, pivotIdx - 1)
+    quickSort(ll, compareFn, pivotIdx + 1, end)
 
 
 class Solution:
-    @staticmethod
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         frequencies: dict[int, int] = {}
         for num in nums:
@@ -25,7 +38,11 @@ class Solution:
             else:
                 frequencies[num] += 1
         sortedFrequency = list(frequencies.items())
-        return []
+        quickSort(sortedFrequency, lambda t1,
+                  t2: t1[1] < t2[1], 0, len(sortedFrequency) - 1)
+
+        return [t[0] for t in sortedFrequency[:k]]
+
 
         # @lc code=end
 if __name__ == "__main__":
