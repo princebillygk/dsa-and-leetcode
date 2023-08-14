8176d3ab6b4542847cfcf7a56f43c52d397a65f6
princebillygk <princebillygk@gmail.com>
2023-08-14 16:17:18 +0600

    Add solution for rotating an array in Python

---
+++ python3/0189.rotate-array/solution.py
@@ -0,0 +1,42 @@
+# Created by Bob at 2023/08/14 12:40
+# leetgo: dev
+# https://leetcode.com/problems/rotate-array/
+
+from typing import *
+from collections import deque
+from leetgo_py import *
+
+# @lc code=begin
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k = k % len(nums) 
+        q:Deque[int] = deque()
+
+        for i in range(len(nums)):
+            swapIdx = (i+k) % len(nums)
+            q.append(nums[swapIdx])
+            if i < k:
+                nums[swapIdx] = nums[i]
+            else: 
+                nums[swapIdx] = q.popleft()
+
+        # # Clearing queue
+        # i = 0
+        # while len(q) > 0:
+        #     nums[i] = q.popleft()
+        #     i+=0
+
+                
+        
+        
+
+# @lc code=end
+
+if __name__ == "__main__":
+    nums: List[int] = deserialize("List[int]", read_line())
+    k: int = deserialize("int", read_line())
+    Solution().rotate(nums, k)
+    ans = nums
+
+    print("\noutput:", serialize(ans))
