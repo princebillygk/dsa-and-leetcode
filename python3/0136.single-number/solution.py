4ad3a7c1a93ea3ef4db0bf30485c9822afdab56d
princebillygk <princebillygk@gmail.com>
2023-08-16 06:23:31 +0600

    solved single number

---
+++ python3/0136.single-number/solution.py
@@ -0,0 +1,25 @@
+# Created by princebillygk at 2023/08/16 05:51
+# leetgo: dev
+# https://leetcode.com/problems/single-number/
+
+from typing import *
+from leetgo_py import *
+
+# @lc code=begin
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        extraDigit = 0
+        for num in nums:
+            extraDigit ^= num
+        return extraDigit
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    nums: List[int] = deserialize("List[int]", read_line())
+    ans = Solution().singleNumber(nums)
+
+    print("\noutput:", serialize(ans))
