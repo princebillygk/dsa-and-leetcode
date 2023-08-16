f89523b6a7d99cdb3ccb251465fdafddf984e60e
princebillygk <princebillygk@gmail.com>
2023-08-16 17:15:29 +0600

    Refactor isValidSudoku method for readability and optimization

--- python3/0036.valid-sudoku/solution.py
+++ python3/0036.valid-sudoku/solution.py
@@ -1,4 +1,4 @@
-# Created by princebillygk at 2023/08/16 08:37
+# Created by Bob at 2023/08/16 15:03
 # leetgo: dev
 # https://leetcode.com/problems/valid-sudoku/
 
@@ -9,7 +9,47 @@ from leetgo_py import *
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+
         
+        for row in range(9):
+            rowSet = set()
+            for col in range(9):
+                val = board[row][col]
+
+                if val in rowSet:
+                    return False
+
+                if val != '.':
+                    rowSet.add(val)
+
+        for row in range(9):
+            colSet = set()
+            for col in range(9):
+                val = board[col][row]
+
+                if val in colSet:
+                    return False
+
+                if val != '.':
+                   colSet.add(val) 
+
+
+
+        for n in range(9):
+            boxSet = set()
+            for k in range(9):
+                r = k // 3 + ((n // 3 )* 3)
+                c = k % 3 + (( n % 3) * 3)
+                val = board[r][c]
+
+                if val in boxSet:
+                    return False
+
+                if val != '.':
+                    boxSet.add(val)
+
+        return True
+ 
 
 # @lc code=end
 
