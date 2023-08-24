4cffe7f4eb5e387c1b1892d5b307e4eac8fb2f90
princebillygk <princebillygk@gmail.com>
2023-08-24 17:54:36 +0600

    I hate myself now

--- python3/0215.kth-largest-element-in-an-array/solution.py
+++ python3/0215.kth-largest-element-in-an-array/solution.py
@@ -9,10 +9,35 @@ import heapq
 # @lc code=begin
 
 
+# This approach gives ttl no matter what I do
 class Solution:
     def findKthLargest(self, nums: List[int], k: int, l: int = 0, r: Optional[int] = None) -> int:
-        return heapq.nlargest(k, nums)[-1]
+        return Solution.quick_select(nums, 0, len(nums) - 1, k - 1)
 
+    @staticmethod
+    def quick_select(nums, left, right, idx) -> int:
+        if left >= right:
+            return nums[idx]
+
+        p = Solution.partition(nums, left, right)
+        if p == idx:
+            return nums[idx]
+        elif p < idx:
+            return Solution.quick_select(nums, p + 1, right, idx)
+        return Solution.quick_select(nums, left, p - 1, idx)
+
+    @staticmethod
+    def partition(nums: List[int], left: int, right: int):
+        pivotValue = nums[right]
+
+        curIdx = left
+        for i in range(left, right):
+            if nums[i] < pivotValue:
+                nums[i], nums[curIdx] = nums[curIdx], nums[i]
+                curIdx += 1
+
+        nums[curIdx], nums[right] = pivotValue, nums[curIdx]
+        return curIdx
 
         # @lc code=end
 if __name__ == "__main__":
