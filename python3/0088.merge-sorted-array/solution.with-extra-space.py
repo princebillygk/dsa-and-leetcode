4e2bfe598dcfa0193a095e1457658aa65a72f9a6
princebillygk <princebillygk@gmail.com>
2023-08-29 13:32:02 +0600

    Merge two sorted arrays into one array

---
+++ python3/0088.merge-sorted-array/solution.with-extra-space.py
@@ -0,0 +1,36 @@
+# Created by Bob at 2023/08/29 12:58
+# leetgo: dev
+# https://leetcode.com/problems/merge-sorted-array/
+
+from typing import *
+from leetgo_py import *
+
+# @lc code=begin
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i = j = 0
+        nums1Copy = nums1.copy()
+
+        while i + j < m + n:
+            print(i + j)
+            if j >= n or i < m and nums1Copy[i] < nums2[j]:
+                nums1[i + j] = nums1Copy[i]
+                i += 1
+            else:
+                nums1[i + j] = nums2[j]
+                j += 1
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    nums1: List[int] = deserialize("List[int]", read_line())
+    m: int = deserialize("int", read_line())
+    nums2: List[int] = deserialize("List[int]", read_line())
+    n: int = deserialize("int", read_line())
+    Solution().merge(nums1, m, nums2, n)
+    ans = nums1
+
+    print("\noutput:", serialize(ans))
