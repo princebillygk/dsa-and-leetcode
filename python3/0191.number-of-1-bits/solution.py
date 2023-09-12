b5996c042c1feedfecd4e905b4ba0f9de2997fb7
princebillygk <princebillygk@gmail.com>
2023-09-12 16:45:49 +0600

    solved count the number of 1

---
+++ python3/0191.number-of-1-bits/solution.py
@@ -0,0 +1,28 @@
+# Created by Bob at 2023/09/12 16:42
+# leetgo: dev
+# https://leetcode.com/problems/number-of-1-bits/
+
+from typing import *
+from leetgo_py import *
+
+# @lc code=begin
+
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count: int = 0
+        while n:
+            if n & 1:
+                count += 1
+            n >>= 1
+        return count
+
+
+# @lc code=end
+
+# Warning: this is a manual question, the generated test code may be incorrect.
+if __name__ == "__main__":
+    n: int = deserialize("int", read_line())
+    ans = Solution().hammingWeight(n)
+
+    print("\noutput:", serialize(ans))
